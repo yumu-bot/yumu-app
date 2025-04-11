fd0bbd64d58bf1bb7f107b16dabff511cc801785
SIyuyuko <3228981717@qq.com>
2025-04-11 19:48:28 +0800

    update

--- App.vue
+++ App.vue
@@ -1,17 +1,22 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-	}
+export default {
+	onLaunch: function () {
+		console.log('App Launch');
+	},
+	onShow: function () {
+		console.log('App Show');
+	},
+	onHide: function () {
+		console.log('App Hide');
+	},
+};
 </script>
 
-<style>
-	/*每个页面公共css */
+<style lang="scss">
+/*每个页面公共css */
+body {
+	padding: 20rpx 10rpx 10rpx 10rpx;
+	box-sizing: border-box;
+	background-color: #e5f6ff;
+}
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,52 +1,62 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/image/avatar-guest.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<!-- App主页 -->
+	<view class="page">
+		<!-- 玩家卡片 -->
+		<view class="player-card">
+			<image class="avatar" src="/static/image/avatar-guest.png"></image>
+			<text class="player-name">{{ title }}</text>
+			<text>
+				<text>1611</text>
+				PP
+			</text>
+		</view>
+		<!-- 二级页面 -->
+		<view>
+			<button type="default">测试按钮1</button>
+			<button type="default">test2</button>
 		</view>
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+<script setup>
+import { ref } from 'vue';
+let title = ref('Player');
 </script>
 
-<style>
-	.content {
+<style lang="scss">
+.page {
+	view.player-card {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
+		background-color: #002295;
+		border-radius: 20rpx;
+		box-shadow: 10rpx 10rpx 20rpx 20rpx rgba(0 0 0 / 0.2);
+		margin: 15rpx 0;
+		padding: 20rpx 0;
+		row-gap: 20rpx;
+		font-size: $uni-font-size-lg;
+		color: #fff;
+		.avatar {
+			height: 200rpx;
+			width: 200rpx;
+		}
+		.player-name + text {
+			text {
+				font-size: 20px !important;
+			}
+		}
 	}
+}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
+.text-area {
+	display: flex;
+	justify-content: center;
+}
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+.title {
+	font-size: 36rpx;
+	color: #8f8f94;
+}
 </style>
